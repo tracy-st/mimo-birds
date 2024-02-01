562c6a91fa177e1e77ab16a1b4a32897626418c0
tracy-st <55551284+tracy-st@users.noreply.github.com>
2024-02-01 10:53:29 -0500

    Update bird images

--- js_objects/script.js
+++ js_objects/script.js
@@ -2,28 +2,28 @@ let chipping = {
     name: "Chipping Sparrow",
     head:"Rusty cap, white stripe over eye, black line through eye",
     breast: "Plain, no streaks or dark central spot",
-    image: "https://www.massaudubon.org/var/site/storage/images/9/3/5/4/1604539-1-eng-US/79d64c123089-DFkforesto-2329.jpg"
+    image: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/297632331/1800"
 }
 
 let tree = {
     name: "American Tree Sparrow",
     head: "Rusty cap, bill dark above and yellow below",
     breast: "Plain with a dark central spot",
-    image: "https://www.allaboutbirds.org/guide/assets/photo/297719081-480px.jpg"
+    image: "https://www.allaboutbirds.org/guide/assets/photo/64971271-480px.jpg"
 }
 
 let house = {
     name: "House Sparrow",
     head: "Gray crown and rusty on back of head; conspicuous black throat",
     breast: "Plain underparts without streaks; conspicuous single white bar on the wing",
-    image: "https://www.massaudubon.org/var/site/storage/images/4/8/9/8/1628984-1-eng-US/d82e5c4bcf1e-4377Mary_Boucher19097-1200x801.jpg"
+    image: "https://www.allaboutbirds.org/guide/assets/photo/305874191-480px.jpg"
 }
 
 let song = {
     name: "Song Sparrow",
     head: "Thick brown throat and mustache stripes, ",
     breast: "Coarse brown streaks",
-    image: "https://maineaudubon.org/wp-content/uploads/2022/10/SongSparrow-768x512.jpg"
+    image: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/66116691/1800"
 }
 
 const gridTemplate = (bird) => `
